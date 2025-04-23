644623669d3399d393e635b8ad3fdce50bb8e763
PaulieScanlon <pauliescanlon@gmail.com>
2025-04-23 11:59:16 +0100

    test: set to serverless dev

--- src/function.js
+++ src/function.js
@@ -1,33 +1,33 @@
-// import { sql } from './db.js';
-
-// export const getDefaultData = async () => {
-//   try {
-//     const result = await sql`SELECT version()`;
-
-//     return {
-//       env: process.env.NODE_ENV,
-//       data: result,
-//     };
-//   } catch (error) {
-//     console.error('Database error:', error);
-//     throw error;
-//   }
-// };
-
-import { pool } from './pg.js';
+import { sql } from './db.js';
 
 export const getDefaultData = async () => {
-  const client = await pool.connect();
   try {
-    const result = await client.query('SELECT version()');
+    const result = await sql`SELECT version()`;
+
     return {
       env: process.env.NODE_ENV,
-      data: result.rows[0],
+      data: result,
     };
   } catch (error) {
     console.error('Database error:', error);
     throw error;
-  } finally {
-    client.release();
   }
 };
+
+// import { pool } from './pg.js';
+
+// export const getDefaultData = async () => {
+//   const client = await pool.connect();
+//   try {
+//     const result = await client.query('SELECT version()');
+//     return {
+//       env: process.env.NODE_ENV,
+//       data: result.rows[0],
+//     };
+//   } catch (error) {
+//     console.error('Database error:', error);
+//     throw error;
+//   } finally {
+//     client.release();
+//   }
+// };
